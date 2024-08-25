214fcf0fffcb693bf0d27b4dcf395fc378872993
hewei <2267849191@qq.com>
2024-08-26 01:27:43 +0800

    Completed long press continuous clicking, achieved exporting the file as a single JSON file, implemented default King of Glory configuration, and added a complete exit button

--- app/src/main/java/com/fde/keyassist/util/FileUtil.java
+++ app/src/main/java/com/fde/keyassist/util/FileUtil.java
@@ -1,7 +1,9 @@
 package com.fde.keyassist.util;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Environment;
+import android.util.Log;
 
 import com.fde.keyassist.entity.AmplifyMappingEntity;
 import com.fde.keyassist.entity.CursorEntity;
@@ -12,19 +14,28 @@ import com.fde.keyassist.entity.Plan;
 import com.fde.keyassist.entity.ScaleMappingEntity;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 
 import org.litepal.LitePal;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -83,23 +94,32 @@ public class FileUtil {
 
             // 获取 SD 卡根目录
             File sdCardRoot = Environment.getExternalStorageDirectory();
-            File outputDir = new File(sdCardRoot, "keyAssist/"+planName);
+            File outputDir = new File(sdCardRoot, "keyAssist/");
 
             if (!outputDir.exists()) {
                 outputDir.mkdirs();
             }
-            // 导出 JSON 文件
-            exportListToJson(curKeyMappingEntity, new File(outputDir, plan.getPlanName()+"_tapClick.json"));
+//            // 导出 JSON 文件
+//            exportListToJson(curKeyMappingEntity, new File(outputDir, plan.getPlanName()+"_tapClick.json"));
+//
+//            exportListToJson(directMappingEntities, new File(outputDir, plan.getPlanName()+"_directClick.json"));
+//
+//            exportListToJson(doubleClickMappingEntityList, new File(outputDir, plan.getPlanName()+"_doubleClick.json"));
+//
+//            exportListToJson(curScaleMappingEntity, new File(outputDir, plan.getPlanName()+"_scaleClick.json"));
+//
+//            exportListToJson(cursorEntities, new File(outputDir, plan.getPlanName()+"_cursorClick.json"));
+//
+//            exportListToJson(amplifyMappingEntities, new File(outputDir, plan.getPlanName()+"_amplifyClick.json"));
 
-            exportListToJson(directMappingEntities, new File(outputDir, plan.getPlanName()+"_directClick.json"));
-
-            exportListToJson(doubleClickMappingEntityList, new File(outputDir, plan.getPlanName()+"_doubleClick.json"));
-
-            exportListToJson(curScaleMappingEntity, new File(outputDir, plan.getPlanName()+"_scaleClick.json"));
-
-            exportListToJson(cursorEntities, new File(outputDir, plan.getPlanName()+"_cursorClick.json"));
-
-            exportListToJson(amplifyMappingEntities, new File(outputDir, plan.getPlanName()+"_amplifyClick.json"));
+            List<Object> list = new ArrayList<>();
+            list.add(curKeyMappingEntity);
+            list.add(directMappingEntities);
+            list.add(doubleClickMappingEntityList);
+            list.add(cursorEntities);
+            list.add(curScaleMappingEntity);
+            list.add(amplifyMappingEntities);
+            exportListToJson(list,new File(outputDir,planName+".json"));
         }
 
 
@@ -107,98 +127,39 @@ public class FileUtil {
 
 
 
-    public static void readJsonFilesFromFolder(File folder) {
-        // 检查文件夹是否存在且是目录
-        if (folder.exists() && folder.isDirectory()) {
-            // 列出文件夹中的所有方案
-            File[] files = folder.listFiles();
-            // 列出方案里面所有文件
-            for(File planName : files){
-                if(planName.exists() && planName.isDirectory()){
-                    // 列出所有文件
-                    File[] keyNames = planName.listFiles();
-                    // 保存方案
-                    Plan plan = new Plan();
-                    plan.setPlanName(planName.getName());
-                    plan.save();
-                    for(File keyName: keyNames){
-                        if (keyName.isFile() && keyName.getName().endsWith(".json")) {
-                            // 读取并处理 JSON 文件
-                                if(keyName.getName().contains("directClick")){
-                                    List<DirectMappingEntity> directMappingEntities = readJsonFile(keyName, new TypeToken<ArrayList<DirectMappingEntity>>(){}.getType());
-                                    if(directMappingEntities != null && !directMappingEntities.isEmpty()){
-                                        for(int i=0;i<directMappingEntities.size();i++){
-                                            DirectMappingEntity directMapping = directMappingEntities.get(i);
-                                            directMapping.assignBaseObjId(0);
-                                            directMapping.setPlanId(plan.getId());
-                                            directMapping.save();
-                                        }
-                                    }
-                                }
-                                if(keyName.getName().contains("doubleClick")){
-                                    List<DoubleClickMappingEntity>  doubleClickMappingEntities= readJsonFile(keyName, new TypeToken<ArrayList<DoubleClickMappingEntity>>(){}.getType());
-                                    if(doubleClickMappingEntities != null && !doubleClickMappingEntities.isEmpty()){
-                                        for(DoubleClickMappingEntity doubleClickMapping : doubleClickMappingEntities){
-                                            doubleClickMapping.assignBaseObjId(0);
-                                            doubleClickMapping.setId(null);
-                                            doubleClickMapping.setPlanId(plan.getId());
-                                            doubleClickMapping.save();
-                                        }
-                                    }
-                                }
-                                if(keyName.getName().contains("scaleClick")){
-                                    List<ScaleMappingEntity> scaleMappingEntities = readJsonFile(keyName, new TypeToken<ArrayList<ScaleMappingEntity>>(){}.getType());
-                                    if(scaleMappingEntities != null && !scaleMappingEntities.isEmpty()){
-                                        for(ScaleMappingEntity scaleMapping : scaleMappingEntities){
-                                            scaleMapping.assignBaseObjId(0);
-                                            scaleMapping.setId(null);
-                                            scaleMapping.setPlanId(plan.getId());
-                                            scaleMapping.save();
-                                        }
-                                    }
-                                }
-                                if(keyName.getName().contains("tapClick")){
-                                    List<KeyMappingEntity> keyMappingEntities = readJsonFile(keyName, new TypeToken<ArrayList<KeyMappingEntity>>(){}.getType());
-                                    if(keyMappingEntities != null && !keyMappingEntities.isEmpty()){
-                                        for(KeyMappingEntity keyMapping : keyMappingEntities){
-                                            keyMapping.assignBaseObjId(0);
-                                            keyMapping.setId(null);
-                                            keyMapping.setPlanId(plan.getId());
-                                            keyMapping.save();
-                                        }
-                                    }
-                                }
-                                if(keyName.getName().contains("cursorClick")){
-                                    List<CursorEntity> cursorEntities = readJsonFile(keyName, new TypeToken<ArrayList<CursorEntity>>(){}.getType());
-                                    if(cursorEntities != null && !cursorEntities.isEmpty()){
-                                        for(CursorEntity cursor : cursorEntities){
-                                            cursor.assignBaseObjId(0);
-                                            cursor.setId(null);
-                                            cursor.setPlanId(plan.getId());
-                                            cursor.save();
-                                        }
-                                    }
-                                }
-                            if(keyName.getName().contains("amplifyClick")){
-                                List<AmplifyMappingEntity> amplifyMappingEntities = readJsonFile(keyName, new TypeToken<ArrayList<AmplifyMappingEntity>>(){}.getType());
-                                if(amplifyMappingEntities != null && !amplifyMappingEntities.isEmpty()){
-                                    for(AmplifyMappingEntity cursor : amplifyMappingEntities){
-                                        cursor.assignBaseObjId(0);
-                                        cursor.setId(null);
-                                        cursor.setPlanId(plan.getId());
-                                        cursor.save();
-                                    }
-                                }
-                            }
-                            }
+    public static void readJsonFilesFromFolder(File folder)  {
+        try {
+            // 检查文件夹是否存在且是目录
+            if (folder.exists() && folder.isDirectory()) {
+                // 列出文件夹中的所有方案
+                File[] files = folder.listFiles();
+                // 列出方案里面所有文件
+                for (File file : files) {
+                    if (file.isFile() && file.getName().endsWith(".json")) {
+                        // 保存方案
+                        Plan plan = new Plan();
+                        int lastIndex = file.getName().lastIndexOf('.');
+                        String prefix = file.getName().substring(0, lastIndex);
+                        plan.setPlanName(prefix);
+                        plan.save();
+                        // 读取并处理 JSON 文件
+                        FileReader reader = new FileReader(file);
+                        JsonElement jsonElement = JsonParser.parseReader(reader);
+                        if(jsonElement.isJsonArray()){
+                            JsonArray jsonArray = jsonElement.getAsJsonArray(); // 全部对象
+                            jsonTokeyMapping(jsonArray.get(0).getAsJsonArray(),plan.getId());
+                            jsonToDirect(jsonArray.get(1).getAsJsonArray(),plan.getId());
+                            jsonToDoubleClickMapping(jsonArray.get(2).getAsJsonArray(),plan.getId());
+                            jsonToCursorEntity(jsonArray.get(3).getAsJsonArray(),plan.getId());
+                            jsonToScale(jsonArray.get(4).getAsJsonArray(),plan.getId());
+                            jsonToAmplify(jsonArray.get(5).getAsJsonArray(),plan.getId());
                         }
+
                     }
-
                 }
+            }
+        }catch (Exception e){
 
-
-        } else {
-            System.out.println("The specified folder does not exist or is not a directory.");
         }
     }
 
@@ -279,6 +240,116 @@ public class FileUtil {
         }
         dir.delete();
     }
+
+    // jsonOBject转为对象
+    public static void jsonTokeyMapping(JsonArray jsonArray,Integer planId){
+        for(int i=0;i<jsonArray.size();i++){
+            JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+            String jsonString = jsonObject.toString();
+            // 使用Gson将JSON字符串转换为Person对象
+            Gson gson = new Gson();
+            KeyMappingEntity keyMapping = gson.fromJson(jsonString, KeyMappingEntity.class);
+            keyMapping.setPlanId(planId);
+            keyMapping.assignBaseObjId(0);
+            keyMapping.save();
+        }
+    }
+
+    public static void jsonToDoubleClickMapping(JsonArray jsonArray,Integer planId){
+        for(int i=0;i<jsonArray.size();i++){
+            JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+            String jsonString = jsonObject.toString();
+            // 使用Gson将JSON字符串转换为Person对象
+            Gson gson = new Gson();
+            DoubleClickMappingEntity keyMapping = gson.fromJson(jsonString, DoubleClickMappingEntity.class);
+            keyMapping.setPlanId(planId);
+            keyMapping.assignBaseObjId(0);
+            keyMapping.save();
+        }
+    }
+
+    public static void jsonToCursorEntity(JsonArray jsonArray,Integer planId){
+        for(int i=0;i<jsonArray.size();i++){
+            JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+            String jsonString = jsonObject.toString();
+            // 使用Gson将JSON字符串转换为Person对象
+            Gson gson = new Gson();
+            CursorEntity keyMapping = gson.fromJson(jsonString, CursorEntity.class);
+            keyMapping.setPlanId(planId);
+            keyMapping.assignBaseObjId(0);
+            keyMapping.save();
+        }
+    }
+
+    public static void jsonToDirect(JsonArray jsonArray,Integer planId){
+        for(int i=0;i<jsonArray.size();i++){
+            JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+            String jsonString = jsonObject.toString();
+            // 使用Gson将JSON字符串转换为Person对象
+            Gson gson = new Gson();
+            DirectMappingEntity keyMapping = gson.fromJson(jsonString, DirectMappingEntity.class);
+            keyMapping.setPlanId(planId);
+            keyMapping.assignBaseObjId(0);
+            keyMapping.save();
+        }
+    }
+
+    public static void jsonToAmplify(JsonArray jsonArray,Integer planId){
+        for(int i=0;i<jsonArray.size();i++){
+            JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+            String jsonString = jsonObject.toString();
+            // 使用Gson将JSON字符串转换为Person对象
+            Gson gson = new Gson();
+            AmplifyMappingEntity keyMapping = gson.fromJson(jsonString, AmplifyMappingEntity.class);
+            keyMapping.setPlanId(planId);
+            keyMapping.assignBaseObjId(0);
+            keyMapping.save();
+        }
+    }
+
+    public static void jsonToScale(JsonArray jsonArray,Integer planId){
+        for(int i=0;i<jsonArray.size();i++){
+            JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+            String jsonString = jsonObject.toString();
+            // 使用Gson将JSON字符串转换为Person对象
+            Gson gson = new Gson();
+            ScaleMappingEntity keyMapping = gson.fromJson(jsonString, ScaleMappingEntity.class);
+            keyMapping.setPlanId(planId);
+            keyMapping.assignBaseObjId(0);
+            keyMapping.save();
+        }
+    }
+
+
+    // 预置json，英雄联盟
+    public static void league(Context context)  {
+        List<Plan> plans = LitePal.where("planName  = ?", "王者荣耀").find(Plan.class);
+        if(plans != null && !plans.isEmpty()){
+            return;
+        }
+        try{
+            AssetManager assetManager = context.getAssets();
+            InputStream inputStream = assetManager.open("HonorofKings.json");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            JsonElement jsonElement = JsonParser.parseReader(bufferedReader);
+            Plan plan = new Plan();
+            plan.setPlanName("王者荣耀");
+            plan.save();
+            if(jsonElement.isJsonArray()){
+                JsonArray jsonArray = jsonElement.getAsJsonArray(); // 全部对象
+                jsonTokeyMapping(jsonArray.get(0).getAsJsonArray(),plan.getId());
+                jsonToDirect(jsonArray.get(1).getAsJsonArray(),plan.getId());
+                jsonToDoubleClickMapping(jsonArray.get(2).getAsJsonArray(),plan.getId());
+                jsonToCursorEntity(jsonArray.get(3).getAsJsonArray(),plan.getId());
+                jsonToScale(jsonArray.get(4).getAsJsonArray(),plan.getId());
+                jsonToAmplify(jsonArray.get(5).getAsJsonArray(),plan.getId());
+            }
+        }catch (Exception e){
+
+        }
+
+    }
+
 
 
 }
