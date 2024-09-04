151faf4ffd4b4291374af3616f84a30c15255d0f
hewei <2267849191@qq.com>
2024-09-04 15:55:32 +0800

    Completed application adaptive sizing, modified some interfaces, and added edge adsorption function

---
+++ app/src/main/java/com/fde/keyassist/util/ScreenUtil.java
@@ -0,0 +1,19 @@
+package com.fde.keyassist.util;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.ActivityManager;
+import android.app.ActivityOptions;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Rect;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class ScreenUtil {
+
+
+
+}
