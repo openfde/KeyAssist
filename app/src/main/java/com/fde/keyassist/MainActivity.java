744394c78dbf1b5b5695a7f9221d8a8c3b7284d0
hewei <2267849191@qq.com>
2024-09-11 08:45:53 +0800

    modify style

--- app/src/main/java/com/fde/keyassist/MainActivity.java
+++ app/src/main/java/com/fde/keyassist/MainActivity.java
@@ -16,8 +16,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import androidx.core.app.ActivityCompat;
