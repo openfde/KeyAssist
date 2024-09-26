ba9b821378212ba25b90b18f4a0b27236d645c97
hewei <2267849191@qq.com>
2024-09-26 15:51:17 +0800

    Keystore: use plugin.keystore

--- app/src/main/java/com/fde/keyassist/util/Constant.java
+++ app/src/main/java/com/fde/keyassist/util/Constant.java
@@ -2,7 +2,7 @@ package com.fde.keyassist.util;
 
 public class Constant {
     public final static Integer TAP_CLICK_EVENT = 1; // 单击事件
-    public final static Integer DOUBLE_CLICK_EVENT = 2; // 双击事件
+    public final static Integer DOUBLE_CLICK_EVENT = 2; // 双击事件 not now,  for continus click
     public final static Integer SWIPE = 3; // 滑动
     public final static Integer DIRECTION_KEY = 4; // 方向键
     public final static Integer DIRECTION_KEY_UP = 5;
